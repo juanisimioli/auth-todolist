899773a19785bb7b538564794ffa172b2b07e1d7
Juani Simioli <juanisimioli@gmail.com>
2024-05-09 12:20:21 -0300

    revert test changes

--- src/api/is.js
+++
@@ -1,7 +0,0 @@
-export const isTest = async (event) =>
-  await fetch(`${process.env.API}${process.env.ENDPOINT}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(event),
-    cache: "no-store",
-  });
