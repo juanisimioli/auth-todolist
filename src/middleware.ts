a568775b5640288b33ecd05582f9b7114924d25c
Juani Simioli <juanisimioli@gmail.com>
2024-05-09 11:15:12 -0300

    test middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,38 @@ import { NextResponse, NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 import { getTokenFromCookie, getJwtSecret, encodeSecret } from "./utils/auth";
 
+export const internalStatServer = async (event: any) =>
+  await fetch(`${process.env.API}${process.env.ENDPOINT}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(event),
+    cache: "no-store",
+  });
+
 /**
  * Middleware for verifying JWT tokens for authentication.
  */
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const tokenJwt = getTokenFromCookie();
 
-  const ip = request.ip;
-  const city = request?.geo?.city;
-  const country = request?.geo?.country;
-  const region = request?.geo?.region;
+  const pathname = request.nextUrl.pathname;
 
-  console.log({ ip, city, country, region });
+  const body = {
+    section: "test juani 2",
+    ip: request.ip,
+    country: request.geo?.country,
+    state: `${request.geo?.region}-${request.geo?.city}`,
+  };
+
+  try {
+    console.log("try", pathname);
+    const res = await internalStatServer(body);
+    console.log("RES!!!", res.status, res);
+  } catch (error) {
+    console.error("loginInfo middleware ERROR", error);
+  } finally {
+    console.log("FINALLY");
+  }
 
   if (!tokenJwt) {
     const redirectUrl = new URL("/login", request.url);
